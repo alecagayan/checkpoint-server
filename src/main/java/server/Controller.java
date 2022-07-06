131a6f6b72db12353846079d40f134aca9355e87
Alec <alecagayan24@gmail.com>
2022-07-06 10:24:51 -0400

    checkin

--- src/main/java/server/Controller.java
+++ src/main/java/server/Controller.java
@@ -3,8 +3,17 @@ package server;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+
+
+import java.util.Collections;
+import java.util.Map;
 
 import org.json.*;
 
@@ -17,17 +26,31 @@ public class Controller {
         return "Hello World!";
     }
 
-    @PostMapping(value = "/signin", consumes = "application/json", produces = "application/json")
-    public String signin(@RequestBody String body) {
-        System.out.println("POST Called with body: " + body);
+    @GetMapping("signin")
+    public String signin(@RequestParam String student_id, @RequestParam String account) {
+        System.out.println("Signin Called with for student_id : " + student_id);
         Database db = new Database();
-        JSONObject json = new JSONObject(body);
-        String student_id = json.getString("student_id");
-        String account = json.getString("account");
 
-        db.checkIn(student_id, account);
 
-        return json.toString();
+        if (db.checkIn(student_id, account) == 0) {
+            return "Signed in " + student_id + " for " + account;
+        } else {
+            return "Error";
+        }
+    }
+
+    @PostMapping(
+    value = "/test", 
+    consumes = MediaType.APPLICATION_JSON_VALUE, 
+    produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> example() {
+        return Collections.singletonMap("key", "value");
+    }
+
+    @ResponseBody
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public String handleHttpMediaTypeNotAcceptableException() {
+        return "acceptable MIME type:" + MediaType.APPLICATION_JSON_VALUE;
     }
 
 }
--- src/main/java/server/Database.java
+++ src/main/java/server/Database.java
@@ -15,7 +15,7 @@ import org.json.JSONObject;
 public class Database {
     private static final String url = "jdbc:mysql://localhost:3306/checkpoint";
     private static final String username = "checkin";
-    private static final String password = "checkout";
+    private static final String password = "Chkpntuser!23";
     private static Connection conn = null;
 
     public Database() {
@@ -34,28 +34,34 @@ public class Database {
         int result = 1;
         int count = 0;
         try {
-            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM ? WHERE student_id = ?");
-            stmt.setString(1, account);
-            stmt.setString(2, student_id);
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM herndonrobotics WHERE student_id = ?");
+            //stmt.setString(1, account);
+            stmt.setString(1, student_id);
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
                 //add one to the count and append current time to the database
                 count = rs.getInt("count") + 1;
-                PreparedStatement stmt2 = conn.prepareStatement("UPDATE ? SET count = ? WHERE student_id = ?");
-                stmt2.setString(1, account);
-                stmt2.setInt(2, count);
-                stmt2.setString(3, student_id);
+                PreparedStatement stmt2 = conn.prepareStatement("UPDATE herndonrobotics SET count = ? WHERE student_id = ?");
+                //stmt2.setString(1, account);
+                stmt2.setInt(1, count);
+                stmt2.setString(2, student_id);
                 stmt2.executeUpdate();
 
                 //append the current date and time to meetings column
-                PreparedStatement stmt3 = conn.prepareStatement("UPDATE ? SET meetings = CONCAT(meetings, ' ', CURRENT_TIMESTAMP) WHERE student_id = ?");
-                stmt3.setString(1, account);
-                stmt3.setString(2, student_id);
+                PreparedStatement stmt3 = conn.prepareStatement("UPDATE herndonrobotics SET meetings = CONCAT(meetings, ',', CURRENT_TIMESTAMP) WHERE student_id = ?");
+                //stmt3.setString(1, account);
+                stmt3.setString(1, student_id);
                 stmt3.executeUpdate();
 
                 result = 0;
 
             } else {
+                //create a new row in the database
+                PreparedStatement stmt4 = conn.prepareStatement("INSERT INTO herndonrobotics (student_id, count, meetings) VALUES (?, 1, CURRENT_TIMESTAMP)");
+                //stmt4.setString(1, account);
+                stmt4.setString(1, student_id);
+                stmt4.executeUpdate();
+
                 result = 1;
             }
         } catch (SQLException e) {
